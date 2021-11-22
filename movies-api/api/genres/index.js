1c55f09c01ee48af44160838ddae0ca856fcfe14
Andrew Ivory <andyivory2016@hotmail.com>
2021-11-22 15:22:50 +0000

    Mongo lab Exercises

--- movies-api/api/genres/index.js
+++ movies-api/api/genres/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
-import {genres} from './genresData';
-//import uniqid from 'uniqid';
+//import {genres} from './genresData';
+import Genre from './genresModel';
+
 
 
 const router = express.Router(); 
-router.get('/', (req, res) => {
-    res.json(genres);
+// Get all users
+router.get('/', async (req, res) => {
+    const genres = await Genre.find();
+    res.status(200).json(genres);
 });
+
 
 export default router;
--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -1,5 +1,7 @@
 import userModel from '../api/users/userModel';
 import users from './users';
+import genresModel from '../api/genres/genresModel';
+import genres from './genres';
 import dotenv from 'dotenv';
 
 
@@ -12,11 +14,25 @@ async function loadUsers() {
     await userModel.deleteMany();
     await userModel.collection.insertMany(users);
     console.info(`${users.length} users were successfully stored.`);
+
   } catch (err) {
     console.error(`failed to Load user Data: ${err}`);
   }
 }
 
+async function loadGenres(){
+  console.log('load genre Data');
+    try{
+      await genresModel.deleteMany();
+      await genresModel.collection.insertMany(genres);
+      console.info(`${genres.length} genres were successfully stored.`);
+
+    } catch(err){
+          console.error('failed to load genre data: ${err');
+    }
+}
+
 if (process.env.SEED_DB) {
   loadUsers();
+  loadGenres();
 }
